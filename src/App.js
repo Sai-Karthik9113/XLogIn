7bbd01c5cdd22d7b83f4c88a0c20af9d13a805f4
Sai-Karthik9113 <sai9898karthik@gmail.com>
2024-10-30 15:12:28 +0530

    feat: modify App Component to work as the login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userName, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const [userAvailable, setUserAvailable] = useState();
+
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!userName || !password) {
+      setUserAvailable();
+    } else {
+        if (userName === 'user' && password === 'password') {
+          setUserAvailable(true);
+        } else {
+          setUserAvailable(false);
+        }
+    }
+  }
+
+
   return (
     <div className="App">
-
+      <h1>Login Page</h1>
+      {userAvailable === false && <p>Invalid username or password</p>}
+      {userAvailable ? (
+        <p>Welcome, {userName}!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:<input type='text' placeholder='username' value={userName} onChange={handleUserNameChange} required />
+          </label>
+          <br />
+          <label>
+            Password:<input type='password' placeholder='password' value={password} onChange={handlePasswordChange} required />
+          </label>
+          <br />
+          <button type='submit'>
+            Submit
+          </button>
+        </form>
+      )}
     </div>
   );
 }
